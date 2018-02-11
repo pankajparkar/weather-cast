2cb52bf41d45a9b7a9c5d20062f7408ce4dc2ba3
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-02-11 18:11:47 +0530

    Fixed API_KEYS

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const COUNTRY_API_ENDPOINT = 'https://battuta.medunes.net/api/country/all/';
-const FORECAST_API_ENDPOINT = 'https://cors-anywhere.herokuapp.com/samples.openweathermap.org/data/2.5/forecast';
+const FORECAST_API_ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast';
 const API_KEY = 'b1b15e88fa797225412429c1c50c122a1';
-const FORECAST_API_URL =`${FORECAST_API_ENDPOINT}?appid=${API_KEY}`;
+const OPEN_WEATHER_API_KEY= '7ec24178bacbeb8537ee4d6dbe6724db';
+const FORECAST_API_URL =`${FORECAST_API_ENDPOINT}?appid=${OPEN_WEATHER_API_KEY}`;
 const COUNTRY_API_URL =`${FORECAST_API_ENDPOINT}?key=${API_KEY}`;
 
 
@@ -15,11 +16,10 @@ export class WeatherService {
 
   getWeatherData(){
     let loc
-    return this.httpClient.get(FORECAST_API_URL, 
-      {
-        params: {q: 'London,us'},
-        headers: new HttpHeaders().set('X-Requested-With', 'XMLHttpRequest')
-      });
+    return this.httpClient.get(FORECAST_API_URL, {
+        params: {q: 'London,us'}
+      }
+    );
   }
 
 }
