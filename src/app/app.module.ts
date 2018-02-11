23dd9a1f80d36eec0f997a255d28658886c60f01
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-02-11 21:57:40 +0530

    Added dropdown service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { CustomMaterialModule } from './custom-material/custom-material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { CustomMaterialModule } from './custom-material/custom-material.module';
 import { NavbarComponent } from './navbar/navbar.component';
-
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { WeatherService } from './services/weather.service';
+import { DropdownService } from './services/dropdown.service';
 
 @NgModule({
   declarations: [
@@ -26,7 +26,8 @@ import { WeatherService } from './services/weather.service';
     HttpClientModule
   ],
   providers: [
-    WeatherService
+    WeatherService,
+    DropdownService
   ],
   bootstrap: [AppComponent]
 })
