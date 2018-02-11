71228776a2c0ed24986b84a7e7e71e8315524cec
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-02-11 22:16:18 +0530

    Added getCities and getCountries method.

--- src/app/services/dropdown.service.ts
+++ src/app/services/dropdown.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const COUNTRY_API_ENDPOINT = 'https://battuta.medunes.net/api/country/all/';
+const CITY_API_ENDPOINT = 'http://battuta.medunes.net/api/city/fr/search/';
+const COUNTRY_API_KEY = 'b1b15e88fa797225412429c1c50c122a1';
+'/'
+const COUNTRY_API_URL =`${COUNTRY_API_ENDPOINT}?key=${COUNTRY_API_KEY}`;
+const CITY_API_URL =`${CITY_API_ENDPOINT}?key=${COUNTRY_API_KEY}`;
 
 @Injectable()
 export class DropdownService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getCities(){
+    return this.httpClient.get(CITY_API_URL, {
+      params: {
+        region: 'pa'
+      }
+    })
+  }
+
+  getCountries(){
+    return this.httpClient.get(COUNTRY_API_URL);
+  }
 
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-const COUNTRY_API_ENDPOINT = 'https://battuta.medunes.net/api/country/all/';
+
 const FORECAST_API_ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast';
-const API_KEY = 'b1b15e88fa797225412429c1c50c122a1';
 const OPEN_WEATHER_API_KEY= '7ec24178bacbeb8537ee4d6dbe6724db';
 const FORECAST_API_URL =`${FORECAST_API_ENDPOINT}?appid=${OPEN_WEATHER_API_KEY}`;
-const COUNTRY_API_URL =`${FORECAST_API_ENDPOINT}?key=${API_KEY}`;
 
 
 @Injectable()
