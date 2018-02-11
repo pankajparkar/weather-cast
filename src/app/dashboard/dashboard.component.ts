59815356daa6fae94d3b01fccefc121a008323a9
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-02-11 18:29:26 +0530

    Displayed real time data defaulted to london city for now

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,37 +8,13 @@ import { WeatherService } from '../services/weather.service';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private weatherService: WeatherService) { }
+  weatherCast: any;
 
-  folders = [
-    {
-      name: 'Photos',
-      updated: new Date('1/1/16'),
-    },
-    {
-      name: 'Recipes',
-      updated: new Date('1/17/16'),
-    },
-    {
-      name: 'Work',
-      updated: new Date('1/28/16'),
-    }
-  ];
-  notes = [
-    {
-      name: 'Vacation Itinerary',
-      updated: new Date('2/20/16'),
-    },
-    {
-      name: 'Kitchen Remodel',
-      updated: new Date('1/18/16'),
-    }
-  ];
+  constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
     this.weatherService.getWeatherData().subscribe(
-      data => console.log(data)
+      weatherCast => this.weatherCast = weatherCast
     );
   }
-
 }
