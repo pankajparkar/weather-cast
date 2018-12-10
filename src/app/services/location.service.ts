1412525ff4bd612f907426cb383844044536ce94
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-12-10 22:53:53 +0530

    Fixed API key and fallback city country issue.

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -4,8 +4,9 @@ import { HttpClient } from '@angular/common/http';
 import { of, Observable, BehaviorSubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
-const IPDATA_API_ENDPOINT = 'https://api.ipdata.co';
+const IPDATA_API_ENDPOINT = 'https://api.ipdata.co/1.1.1.1?api-key=150731c1daac573655cad56e9fce2f193d9e5f66a0954e400b465277';
 const FALLBACK_CITY = 'Mumbai';
+const FALLBACK_COUNTRY = 'IN';
 
 @Injectable()
 export class LocationService {
@@ -31,8 +32,9 @@ export class LocationService {
         .pipe(
           tap((ipData: any) => {
             // TODO fallback to be removed
-            if (ipData.city === 'Ghatkopar') {
+            if (!ipData.city || ipData.city === 'Ghatkopar') {
               ipData.city = FALLBACK_CITY;
+              ipData.country_code = FALLBACK_COUNTRY;
             }
             this.ipData$.next(ipData);
             this.ipData = ipData;
